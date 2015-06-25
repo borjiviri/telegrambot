91babfb4b4b32cadddc7f6fd625dddbccb85efae
Borja <borja@libcrack.so>
2015-06-26 01:58:53 +0200

    Added class Command aiming to implement bot commands easily

---
+++ Command.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# borja@libcrack.so
+# vie jun 26 01:50:53 CEST 2015
+
+class Command(object):
+
+    def __init__(self):
+        self.name = 'magic'
+        self.action = 'send_message'
+        self.description = 'command description'
+
+    def action(self):
+        pass
+
+    def help(self):
+        pass
+
