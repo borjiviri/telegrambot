852149a4c48a1dd8c956b781bafb787fe4db6394
Borja <borja@libcrack.so>
2015-06-26 07:44:49 +0200

    Daemon signal handling implementation

--- daemon.py
+++ daemon.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-#
 # Date: jue dic 22 04:54:06 CET 2011
 # Author: borja@libcrack.so
+# http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
 
 import atexit
 import signal
@@ -13,34 +13,34 @@ from Logger import Logger
 logger = Logger.logger
 
 class Daemon(object):
-    """
+    '''
     This is a generic daemon class, supporting start,stop,status and pidfile methods.
     To daemonize a class, simply inherits from Daemon and override the run() method.
-    """
+    '''
     def __init__ (self, pidfile='/tmp/pidfile', stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
         self.stdin = stdin
         self.stdout = stdout
         self.stderr = stderr
         self.pidfile = pidfile
 
-    def daemonize (self):
-        """
-        do the UNIX double-fork magic, see Stevens' "Advanced
-        Programming in the UNIX Environment" for details (ISBN 0201563177)
+    def _daemonize (self):
+        '''
+        do the UNIX double-fork magic, see Stevens' 'Advanced
+        Programming in the UNIX Environment' for details (ISBN 0201563177)
         http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
-        """
+        '''
         try:
             pid = os.fork()
             if pid > 0:
-                # exit first parent
-                sys.exit(0)
+                sys.exit(0) # exit first parent
         except OSError as e:
             logger.error('fork #1 failed: {0} ({1})'.format(e.errno, e.strerror))
             sys.exit(1)
 
         # decouple from parent environment
-        # os.chdir('/')         # TODO: chdir to daemon's home directory
-        os.chdir(os.getcwd())   # TODO: chdir to daemon's home directory
+        # TODO: chdir to daemon's home directory
+        # os.chdir('/')
+        os.chdir(os.getcwd())
         os.setsid()
         os.umask(0)
 
@@ -48,13 +48,12 @@ class Daemon(object):
         try:
             pid = os.fork()
             if pid > 0:
-                # exit from second parent
-                sys.exit(0)
+                sys.exit(0) # exit from second parent
         except OSError as e:
             logger.error('fork #2 failed: {0} ({1})'.format(e.errno, e.strerror))
             sys.exit(1)
 
-        # XXX AVOIDED as the Logger class will manage it
+        # AVOIDED as the Logger class will manage it
         # # redirect standard file descriptors
         # sys.stdout.flush()
         # sys.stderr.flush()
@@ -69,7 +68,7 @@ class Daemon(object):
         #     os.dup2(se.fileno(), sys.stderr.fileno())
 
         # write pidfile
-        atexit.register(self.delpid)
+        atexit.register(self._delpid)
         pid = str(os.getpid())
         try:
             f = open(self.pidfile,'w+')
@@ -80,15 +79,35 @@ class Daemon(object):
             f.close()
             logger.info('Written pid {0} in file {1}'.format(pid,self.pidfile))
 
+        logger.info('Configuring signal handler')
+        signal.signal(signal.SIGALRM, self._signal_handler)
 
-    def delpid (self):
+    def _delpid (self):
         os.remove(self.pidfile)
 
+    def _run (self):
+        '''
+        Override me
+        Called after start() or restart() methods
+        '''
+        raise NotImplementedError
+
+    def _set_signals():
+        original_sigint = signal.getsignal(signal.SIGINT)
+        bound_signal_handler = lambda signum, frame: self._signal_handler(
+                signum, frame, original_sigint)
+        signal.signal(signal.SIGHUP,  signal.SIG_IGN)
+        signal.signal(signal.SIGINT,  bound_signal_handler)
+        signal.signal(signal.SIGTERM, bound_signal_handler)
+        signal.signal(signal.SIGALRM, bound_signal_handler)
+
+    def  _signal_handler(self):
+        logger.debug('Signal handler called with signal {0}'.format(signum))
 
     def start (self):
-        """
+        '''
         Start the daemon
-        """
+        '''
         # Check for a pidfile to see if the daemon already runs
         try:
             pf = open(self.pidfile,'r')
@@ -103,14 +122,13 @@ class Daemon(object):
             sys.exit(1)
 
         # Start the daemon
-        self.daemonize()
-        self.run()
-
+        self._daemonize()
+        self._run()
 
     def stop (self):
-        """
+        '''
         Stop the daemon
-        """
+        '''
         # Get the pid from the pidfile
         try:
             pf = open(self.pidfile,'r')
@@ -138,18 +156,10 @@ class Daemon(object):
                     logger.error('{0}'.format(err))
                     sys.exit(1)
 
-
     def restart (self):
-        """
+        '''
         Restart the daemon
-        """
+        '''
         self.stop()
         self.start()
-
-
-    def run (self):
-        """
-        Override me
-        Called after start() or restart() methods
-        """
 
