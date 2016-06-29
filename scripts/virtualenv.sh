ae1ab20768479c234fae8a78722e8040e854ca34
/dev/null <libcrack@users.noreply.github.com>
2016-06-29 15:48:50 +0200

    Mod: Improvements in virtual env shell script

--- scripts/virtualenv.sh
+++ scripts/virtualenv.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-[[ -d venv ]] || {
-    virtualenv -p /usr/bin/python3.4 venv
+# 28/05/2016 16:24:27
+# devnull@libcrack.so
+
+VENV="venv"
+V=3
+[[ -n "${1}" ]] && V="${1}"
+[[ "$BASH_SOURCE" == "${0}" ]] && {
+    myself="$(readlink -m ${0#-*})"
+    echo "Usage: . ${myself}" > /dev/stderr
+    exit 1
+} || {
+    [[ -d "${VENV}" ]] || {
+        echo ">> Creating virtual environment at \"${VENV}\""
+        "virtualenv${V}" -p "/usr/bin/python${V}" "${VENV}"
+    }
+    echo ">> Entering virtual environment \"${VENV}\"" > /dev/stdout
+    source "${VENV}/bin/activate"
 }
-source venv/bin/activate
